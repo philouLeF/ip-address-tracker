c765b3eb67a4f3ab8b98da782a2d96d124b5d24d
philouLeF <delcroixphilippe73@gmail.com>
2023-08-26 20:59:37 +0200

    init map

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,38 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var L = require("leaflet");
+const L = require("leaflet");
+const axios_1 = require("axios");
+let map;
+let marker;
+function fetchIPInfo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const apiKey = "at_FbBf4w6Dv8aXe0m1htLBONR8Ph71V";
+        const response = yield axios_1.default.get(`https://geo.ipify.org/api/v1?apiKey=${apiKey}`);
+        const data = response.data;
+        const { ip, location } = data;
+        console.log(response);
+        if (marker) {
+            marker.setLatLng([location.lat, location.lng]);
+        }
+        else {
+            marker = L.marker([location.lat, location.lng]).addTo(map);
+        }
+    });
+}
 function initMap() {
-    var map = L.map("map").setView([51.505, -0.09], 13);
+    map = L.map("map").setView([51.505, -0.09], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 }
-document.addEventListener("DOMContentLoaded", function (event) {
+document.addEventListener("DOMContentLoaded", (event) => {
     initMap();
+    fetchIPInfo();
 });
--- script.ts
+++ script.ts
@@ -1,10 +1,40 @@
 import * as L from "leaflet";
+import axios from "axios";
 
-function initMap(): void {
-  const map = L.map("map").setView([51.505, -0.09], 13);
+interface IPInfo {
+  ip: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
+let map: L.Map;
+let marker: L.Marker;
+
+function initMap(lat: number = 51.505, lng: number = -0.09): void {
+  map = L.map("map").setView([lat, lng], 13);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+}
+
+async function fetchIPInfo(): Promise<void> {
+  const apiKey = "at_FbBf4w6Dv8aXe0m1htLBONR8Ph71V";
+  const response = await axios.get(
+    `https://geo.ipify.org/api/v1?apiKey=${apiKey}`
+  );
+  const data: IPInfo = response.data;
+  const { ip, location } = data;
+
+  if (marker) {
+    marker.setLatLng([location.lat, location.lng]);
+  } else {
+    marker = L.marker([location.lat, location.lng]).addTo(map);
+  }
+
+  map.setView([location.lat, location.lng], 13);
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
   initMap();
+  fetchIPInfo();
 });
